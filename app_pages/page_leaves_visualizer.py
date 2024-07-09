ffed804c3a9998aecf0ec4d2b2509cd651d89b16
Alihan Teker <alihan.teker@hotmail.com>
2024-07-09 11:13:04 +0000

    Fix typo in page_leaves_visualizer_body function

--- app_pages/page_leaves_visualizer.py
+++ app_pages/page_leaves_visualizer.py
@@ -43,7 +43,7 @@ def page_leaves_visualizer_body():
       my_data_dir = 'inputs/cherry-leaves_dataset/cherry-leaves'
       labels = os.listdir(my_data_dir+ '/validation')
       label_to_display = st.selectbox(label="Select label", options=labels, index=0)
-      if st.button"Create Montage"):      
+      if st.button("Create Montage"):      
         image_montage(dir_path= my_data_dir + '/validation',
                       label_to_display=label_to_display,
                       nrows=8, ncols=3, figsize=(10,25))
